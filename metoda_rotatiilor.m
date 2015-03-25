b18eeb5540e6daa23c6f505af5101ec31b71902d
Dragos Alin <r.dragos0@gmail.com>
2015-03-25 02:17:54 +0200

    done. next one

--- metoda_rotatiilor.m
+++ metoda_rotatiilor.m
@@ -1,23 +1,36 @@
 
+
+function retval = my_norm(A)
+  num_rows = size(A)(1);
+  num_columns = size(A)(2);
+
+  retval = 0;
+
+  for i = 1:num_rows
+    for j = 1:num_columns
+      if i ~= j
+        retval += A(i,j) * A(i, j);
+      end
+    end
+  end
+  retval = sqrt(retval);
+end
 function metoda_rotatii(A, B)
   %no idea why this works
 	how_many = 0;
 
   num_rows = num_columns = size(A)(1);
   toy_matrix = A;
-  printf("IV Matrix");
-
-	while(norm(toy_matrix, "fro") > 1e-10)
-    norm(toy_matrix, "fro");
+	while(my_norm(toy_matrix) > 1e-10)
     how_many += 1;
     mx_matrix = 0;
 
     px = 0; py = 0;
 
     for i = 1:num_rows
-      for j = 1:num_columns
-        if mx_matrix < toy_matrix(i,j)
-          mx_matrix = toy_matrix(i,j);
+      for j = i+1:num_columns
+        if mx_matrix < abs(toy_matrix(i,j))
+          mx_matrix = abs(toy_matrix(i,j));
           px = i;
           py = j;
         end
@@ -36,7 +49,7 @@ function metoda_rotatii(A, B)
     for j = 1: num_columns
       if (j ~= px)
         if (j ~= py)
-          partial_copy(px, j) = partial_copy(j, px) = cos_theta * toy_matrix(px, j) + sin_theta * toy_matrix(py, py);
+          partial_copy(px, j) = partial_copy(j, px) = cos_theta * toy_matrix(px, j) + sin_theta * toy_matrix(py, j);
           partial_copy(py, j) = partial_copy(j, py) = -sin_theta * toy_matrix(px, j) + cos_theta * toy_matrix(py, j);
         end
       end
@@ -52,19 +65,22 @@ function metoda_rotatii(A, B)
 
   %toy_matrix
   [V, lambda] = eig(A);
-  V;
-  lambda;
-  toy_matrix;
+  printf("Octave gives me; then Octave gives me; \n");
+  lambda = sort(diag(lambda));
+  fake_lambda = sort(diag(toy_matrix));
+  for i = 1:size(lambda)(1)
+    printf("%f ----------------- %f\n", lambda(i), fake_lambda(i));
+  end
   A * V;
-  norm(toy_matrix, "fro")
 
 endfunction
 
 
 
-test = [10];
+test = [10; 20; 50; 100];
 for idx = 1:size(test)(1)
 	%equation matrix
+  printf("Running on test %d \n", test(idx));
   dimension = test(idx);
 	A = eye(test(idx)) * 2;
   for i = 1:(dimension - 1)
@@ -73,7 +89,5 @@ for idx = 1:size(test)(1)
   end
 	%column vector
 	B = ones(test(idx))(:,1);
-  X = A \ B
-  A * X - B
 	metoda_rotatii(A, B);
 endfor
